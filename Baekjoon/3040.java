ce6db443440ee3759260b2b4a5a92f25f05b8994
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-08-11 21:27:16 +0900

    220811

---
+++ Baekjoon/3040.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백설공주와 일곱 난쟁이
+
+public class Main {
+
+	static int n = 9;
+	static int r = 7;
+	static int[] arr = new int[n];
+	static int[] selected = new int[r];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int i = 0; i < 9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		combination(0, 0);
+
+	}
+
+	private static void combination(int idx, int count) {
+
+		if (count == r) {
+
+			int sum = 0;
+
+			for (int num : selected) {
+				sum += num;
+			}
+
+			if (sum == 100) {
+
+				for (int num : selected) {
+					sb.append(num).append("\n");
+				}
+
+				System.out.println(sb);
+
+			}
+
+			return;
+
+		}
+
+		for (int i = idx; i < n; i++) {
+			selected[count] = arr[i];
+			combination(i + 1, count + 1);
+		}
+
+	}
+
+}
