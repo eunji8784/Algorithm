c46cece6fba1def76efc3f7dfaf46fa7b53695b5
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-08-27 23:58:13 +0900

    220826

--- Baekjoon/17471.java
+++ Baekjoon/17471.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main_17471 {
+public class Main {
 
 	static int N;
 	static int[] population;
