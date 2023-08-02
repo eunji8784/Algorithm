af07bdcdcba37171e4ca4f88aa477b861cb5c1af
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-02 17:39:01 +0900

    BOJ_1874_스택 수열.java

---
+++ 스택/BOJ_1874_스택 수열.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int idx = 0;
+		int num = 1;
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+
+			if (num > n) {
+				break;
+			}
+
+			stack.push(num);
+			sb.append("+\n");
+			num++;
+
+			while (true) {
+
+				if (arr[idx] == stack.peek()) {
+					stack.pop();
+					sb.append("-\n");
+					idx++;
+					if (stack.empty()) {
+						break;
+					}
+				} else {
+					if (arr[idx] < stack.peek()) {
+						System.out.println("NO");
+						System.exit(0);
+					}
+					break;
+				}
+
+			}
+
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+
+	}
+
+}
