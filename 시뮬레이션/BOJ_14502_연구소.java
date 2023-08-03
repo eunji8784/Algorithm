480696e66cabfb4657184bb5842a97d7baa29aa6
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-03 19:36:55 +0900

    BOJ_14502_연구소.java

--- 시뮬레이션/BOJ_14502_연구소.java
+++ 시뮬레이션/BOJ_14502_연구소.java
@@ -7,9 +7,9 @@ public class Main {
 	static int[] dy = { 0, 0, -1, 1 };
 	static int N, M, max, blankCnt;
 	static int[][] map;
-	static Location[] selected;
-	static ArrayList<Location> blankLoc;
-	static Queue<Location> virusLoc;
+	static Location[] selectedLoc; // 벽을 세울 칸의 위치
+	static ArrayList<Location> blankLoc; // 빈 칸(0)의 위치
+	static Queue<Location> virusLoc; // 바이러스가 존재하는 칸(2)의 위치
 
 	static class Location {
 		int x, y;
@@ -26,19 +26,19 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		map = new int[N][M];
-		selected = new Location[3];
+		selectedLoc = new Location[3];
 		blankLoc = new ArrayList<>();
 		virusLoc = new LinkedList<>();
 		max = Integer.MIN_VALUE;
-		
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == 0) {
-					blankCnt++;
+					blankCnt++; // 빈 칸의 수를 센다.
 					blankLoc.add(new Location(i, j));
 				}
 				if (map[i][j] == 2) {
@@ -46,9 +46,9 @@ public class Main {
 				}
 			}
 		}
-		
+
 		combination(0, 0);
-		
+
 		System.out.println(max);
 		br.close();
 	}
@@ -56,17 +56,17 @@ public class Main {
 	private static void combination(int index, int count) {
 
 		if (count == 3) {
-			max = Math.max(max, bfs(selected));
+			max = Math.max(max, bfs(selectedLoc));
 			return;
 		}
 
 		for (int i = index; i < blankLoc.size(); i++) {
-			selected[count] = blankLoc.get(i);
+			selectedLoc[count] = blankLoc.get(i);
 			combination(i + 1, count + 1);
 		}
 
 	}
-	
+
 	private static void copyMap(int[][] backup, int[][] map) {
 
 		for (int i = 0; i < N; i++) {
@@ -80,40 +80,41 @@ public class Main {
 	private static int bfs(Location[] selected) {
 
 		int[][] backup = new int[N][M];
-		copyMap(backup, map);
-		int safeZone = blankCnt;
-		
+		copyMap(backup, map); // map의 복사본 생성
+
+		int safeZone = blankCnt; // 안전 영역 크기
+
 		// 선택된 3개의 위치에 벽 세우기
 		for (Location loc : selected) {
 			backup[loc.x][loc.y] = 1;
-			safeZone--;
+			safeZone--; // 벽을 세웠으므로 안전 영역의 크기를 1 줄인다.
 		}
-		
-		Queue<Location> que = new LinkedList<>(virusLoc);
-		
+
+		Queue<Location> que = new LinkedList<>(virusLoc); // 바이러스 위치 복사
+
 		while (!que.isEmpty()) {
-			
+
 			Location virus = que.poll();
 			int x = virus.x;
 			int y = virus.y;
-			
+
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
-				
+
 				if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
 					continue;
 				}
-				
+
 				if (backup[nx][ny] == 0) {
 					safeZone--;
 					backup[nx][ny] = 2;
 					que.offer(new Location(nx, ny));
 				}
 			}
-			
+
 		}
-		
+
 		return safeZone;
 
 	}
