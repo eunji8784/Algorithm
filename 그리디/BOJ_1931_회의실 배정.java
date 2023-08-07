9ffea2da16a8639b387646fbe4598837aca07101
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-07 11:02:31 +0900

    BOJ_1931_회의실 배정.java

---
+++ 그리디/BOJ_1931_회의실 배정.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Integer[][] arr = new Integer[N][2];
+
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr, new Comparator<Integer[]>() {
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				if (o1[1] == o2[1]) { // 끝나는 시간이 같다면 시작하는 시간 기준으로 오름차순 정렬
+					return Integer.compare(o1[0], o2[0]);
+				}
+				return Integer.compare(o1[1], o2[1]); // 시작하는 시간 기준으로 오름차순 정렬
+			}
+		});
+
+		int answer = 0;
+		int end = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (arr[i][0] >= end) {
+				answer++;
+				end = arr[i][1];
+			}
+		}
+
+		System.out.println(answer);
+		br.close();
+
+	}
+
+}
