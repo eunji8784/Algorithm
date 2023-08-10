ba3ace4a7641ec3dcc03b8ee85b44cc0309458b3
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-11 01:07:00 +0900

    Softeer_Practice_지우는 소수를 좋아해.java

--- 소프티어 연습문제/Softeer_Practice_지우는 소수를 좋아해.java
+++ 소프티어 연습문제/Softeer_Practice_지우는 소수를 좋아해.java
@@ -1,69 +1,88 @@
 import java.util.*;
 import java.io.*;
 
+// 다익스트라 알고리즘 
 public class Main
 {
+    private static final int INF = 1000000000;
+
     private static int N, M, minLevel = Integer.MAX_VALUE;
-    private static ArrayList<Integer>[] adjLst;
-    private static int[][] levelWeight;
+    private static ArrayList<Edge>[] adjLst;
+    private static TreeSet<Integer> levels = new TreeSet<>();
+
+    private static class Edge implements Comparable<Edge> {
+        int node, cost;
+
+        public Edge (int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
 
     public static void main(String args[]) throws IOException
     {
         getInput();
-        getMinLevel();
-        getAnswer();
-        System.out.println(minLevel);
+        solution();
     }
 
-    private static void getMinLevel() {
-        Set<Integer> set = new HashSet<>();
-        boolean[] vst = new boolean[N + 1];
-        dfs(1, set, 0, vst);
-    }
-
-    private static void dfs(int v, Set<Integer> set, int level, boolean[] vst) {
-        if (level >= minLevel) {
-            return;
-        }
-
-        if (v == N) {
-            minLevel = Math.min(minLevel, level);
-            return;
-        }
-
-        vst[v] = true;
-        set.add(v);
-
-        for (int next : adjLst[v]) {
-            if (!vst[next]) {
-                dfs(next, set, Math.max(level, levelWeight[v][next]), vst);
+    private static void solution() {
+        for (int level : levels) {
+            if (dijkstra(level)) {
+                level++; // 'X레벨 이하 지원자는 오지 마시오.'
+                while (!isPrime(level)) {
+                    level++;
+                }
+                System.out.println(level);
+                System.exit(0);
             }
         }
-
-        vst[v] = false;
-        set.remove(v);
     }
 
-    private static void getAnswer() {
-        minLevel++; // 'X레벨 이하 지원자는 오지 마시오'
-        if (!isPrime()) {
-            while (!isPrime()) {
-                minLevel++;
-            }
-        }
-        return;
-    }
-
-    private static boolean isPrime() {
-        if (minLevel <= 1) {
+    private static boolean isPrime(int level) {
+        if (level <= 1) {
             return false;
         }
-        for (int i = 2; i * i <= minLevel; i++) {
-            if (minLevel % i == 0) {
+        for (int i = 2; i * i <= level; i++) {
+            if (level % i == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static boolean dijkstra(int levelLimit) {
+        int[] distance = new int[N + 1];
+        Arrays.fill(distance, INF);
+        distance[1] = 0;
+
+        PriorityQueue<Edge> que = new PriorityQueue<>();
+        que.offer(new Edge(1, 0));
+
+        while (!que.isEmpty()) {
+            Edge current = que.poll();
+
+            if (current.node == N) {
+                return true;
+            }
+
+            if (distance[current.node] < current.cost) {
+                continue;
+            }
+
+            for (Edge edge : adjLst[current.node]) {
+                if (edge.cost <= levelLimit && current.cost + edge.cost < distance[edge.node]) {
+                    distance[edge.node] = current.cost + edge.cost;
+                    que.offer(new Edge(edge.node, distance[edge.node]));
+                }
+            }
+        }
+
+        return false;
     }
 
     private static void getInput() throws IOException {
@@ -72,7 +91,7 @@ public class Main
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        levelWeight = new int[N + 1][N + 1];
+      
         adjLst = new ArrayList[N + 1];
 
         for (int i = 1; i <= N; i++) {
@@ -85,16 +104,11 @@ public class Main
             int to = Integer.parseInt(st.nextToken());
             int level = Integer.parseInt(st.nextToken());
             
-            if (levelWeight[from][to] != 0) {
-                level = Math.min(levelWeight[from][to], level);
-            } else {
-                adjLst[from].add(to);
-                adjLst[to].add(from);
-            }
-            levelWeight[from][to] = level;
-            levelWeight[to][from] = level;
+            adjLst[from].add(new Edge(to, level));
+            adjLst[to].add(new Edge(from, level));
+            levels.add(level);
         }
-
+        
         br.close();
     }
 }
