7fda0c7658e6a8f6dec918c6ee6e2e96e916df1f
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-14 19:39:16 +0900

    BOJ_15664_N과 M (10).java

---
+++ 백트래킹/BOJ_15664_N과 M (10).java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int N, M;
+	private static int[] arr, selected;
+
+	public static void main(String[] args) throws IOException {
+		getInput();
+		combination(0, 0);
+	}
+
+	private static void combination(int count, int index) {
+		if (count == M) {
+			printSeleceted(selected);
+			return;
+		}
+
+		int prev = -1;
+
+		for (int i = index; i < N; i++) {
+			if (prev != arr[i]) {
+				selected[count] = arr[i];
+				prev = arr[i];
+				combination(count + 1, i + 1);
+			}
+		}
+	}
+
+	private static void printSeleceted(int[] selected) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			sb.append(selected[i] + " ");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static void getInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		selected = new int[M];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		br.close();
+	}
+}
