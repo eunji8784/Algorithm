9fd39ed969de3b9dba1e265c770a2d8c7469ac13
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-19 06:40:13 +0900

    BOJ_1676_팩토리얼 0의 개수.java

---
+++ 수학/BOJ_1676_팩토리얼 0의 개수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int n = Integer.parseInt(br.readLine());	
+		int result = 0;
+		
+		for (int i = 5; i <= n; i *= 5) {
+			result += n / i;
+		}
+		
+		System.out.println(result);
+		
+		br.close();
+	}
+
+}
