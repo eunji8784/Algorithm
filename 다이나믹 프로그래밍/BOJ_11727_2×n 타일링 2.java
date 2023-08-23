be272291ec40f849092ee9c763dd5b9c75fd2d71
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-08-23 23:57:21 +0900

    BOJ_11727_2×n 타일링 2.java

--- 다이나믹 프로그래밍/BOJ_11727_2×n 타일링 2.java
+++ 다이나믹 프로그래밍/BOJ_11727_2×n 타일링 2.java
@@ -1,20 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int n = Integer.parseInt(br.readLine());
-		
+
 		int[] dp = new int[n + 1];
 		dp[1] = 1;
-		dp[2] = 3;
-		
-		for (int i = 3; i <= n; i++) {
-			dp[i] = (dp[i - 1] % 10007 + 2 * dp[i - 2] % 10007) % 10007;
+
+		if (n > 1) {
+			dp[2] = 3;
+			for (int i = 3; i <= n; i++) {
+				dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+				// dp[i] = (dp[i - 1] % 10007 + 2 * dp[i - 2] % 10007) % 10007;
+			}
 		}
-		
+
 		System.out.println(dp[n]);
 		br.close();
 	}
