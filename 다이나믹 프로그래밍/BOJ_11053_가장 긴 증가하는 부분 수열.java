d48d90285bc1b2cba843c912162b90e0f77c233e
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-09-02 15:15:07 +0900

    BOJ_11053_가장 긴 증가하는 부분 수열.java

---
+++ 다이나믹 프로그래밍/BOJ_11053_가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		int answer = -1;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i] && dp[i] <= dp[j]) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+			answer = Math.max(answer, dp[i]);
+		}
+		
+		System.out.println(answer);
+		br.close();
+	}
+	
+}
