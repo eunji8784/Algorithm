9c68bb59523d228eb5b0e266e2469df8d8d0ec4f
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-10-08 18:17:36 +0900

    PRG_Lv1_모의고사.java

--- 프로그래머스/코딩테스트 연습/코딩테스트 고득점 Kit/완전탐색/PRG_Lv1_모의고사.java
+++ 프로그래머스/코딩테스트 연습/코딩테스트 고득점 Kit/완전탐색/PRG_Lv1_모의고사.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// stream()을 사용하여 List를 배열로 변환
 class Solution {
     public int[] solution(int[] answers) {
         List<Integer> list = new ArrayList<>();
@@ -41,3 +42,52 @@ class Solution {
 // 테스트 12 〉	통과 (2.50ms, 79.6MB)
 // 테스트 13 〉	통과 (2.32ms, 74.3MB)
 // 테스트 14 〉	통과 (2.75ms, 75.8MB)
+
+// 반복문 사용해 List를 배열로 변환
+// class Solution {
+//     public int[] solution(int[] answers) {
+//         List<Integer> list = new ArrayList<>();
+        
+//         int[] arr1 = {1, 2, 3, 4, 5};
+//         int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
+//         int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+//         int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+//         int len1 = arr1.length, len2 = arr2.length, len3 = arr3.length;
+        
+//         for (int i = 0; i < answers.length; i++) {
+//             if (answers[i] == arr1[i % len1]) cnt1++;
+//             if (answers[i] == arr2[i % len2]) cnt2++;
+//             if (answers[i] == arr3[i % len3]) cnt3++;
+//         } 
+        
+//         int max = Math.max(cnt1, Math.max(cnt2, cnt3));
+        
+//         if (cnt1 == max) list.add(1);
+//         if (cnt2 == max) list.add(2);
+//         if (cnt3 == max) list.add(3);
+        
+//         int[] answer = new int[list.size()];
+        
+//         for (int i = 0; i < list.size(); i++) {
+//             answer[i] = list.get(i);
+//         }
+
+//         return answer;
+//     }
+// }
+
+// 테스트 1 〉	통과 (0.04ms, 74.9MB)
+// 테스트 2 〉	통과 (0.04ms, 73.7MB)
+// 테스트 3 〉	통과 (0.05ms, 74.7MB)
+// 테스트 4 〉	통과 (0.06ms, 79MB)
+// 테스트 5 〉	통과 (0.06ms, 76.6MB)
+// 테스트 6 〉	통과 (0.06ms, 73.2MB)
+// 테스트 7 〉	통과 (0.29ms, 84.8MB)
+// 테스트 8 〉	통과 (0.20ms, 76.9MB)
+// 테스트 9 〉	통과 (0.50ms, 75.5MB)
+// 테스트 10 〉	통과 (0.45ms, 71.5MB)
+// 테스트 11 〉	통과 (0.86ms, 82.1MB)
+// 테스트 12 〉	통과 (0.61ms, 79.7MB)
+// 테스트 13 〉	통과 (0.10ms, 71.3MB)
+// 테스트 14 〉	통과 (0.57ms, 71.1MB)
