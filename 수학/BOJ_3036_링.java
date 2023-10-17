fb891bfc2d3503cff37017190722db33547d67d6
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-10-17 17:54:34 +0900

    BOJ_3036_링.java

--- 수학/BOJ_3036_링.java
+++ 수학/BOJ_3036_링.java
@@ -6,12 +6,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-    StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int first = Integer.parseInt(st.nextToken());
 		
 		StringBuilder answer = new StringBuilder();
 		
-    for (int i = 0; i < N - 1; i++) {
+		for (int i = 0; i < N - 1; i++) {
 			int other = Integer.parseInt(st.nextToken());
 			int gcd = gcd(first, other);
 			answer.append(first / gcd).append("/").append(other / gcd).append("\n");
