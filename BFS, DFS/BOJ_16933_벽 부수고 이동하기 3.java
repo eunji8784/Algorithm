d92034f5250a5532529bb9fde878e77668921489
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-10-26 05:36:50 +0900

    BOJ_16933_벽 부수고 이동하기 3.java

---
+++ BFS, DFS/BOJ_16933_벽 부수고 이동하기 3.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static final int[] DX = { -1, 1, 0, 0 };
+	private static final int[] DY = { 0, 0, -1, 1 };
+	
+	private static int N, M, K;
+	private static int[][] map;
+	
+	private static class Location {
+		int x, y, k, dist;
+		boolean isDaytime;
+		
+		public Location(int x, int y, int k, int dist, boolean isDaytime) {
+			this.x = x;
+			this.y = y;
+			this.k = k;
+			this.dist = dist;
+			this.isDaytime = isDaytime;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		map = new int[N + 1][M + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			String str = br.readLine();
+			for (int j = 1; j <= M; j++) {
+				map[i][j] = str.charAt(j - 1) - '0';
+			}
+		}
+		
+		System.out.println(bfs());
+		br.close();
+	}
+	
+	private static int bfs() {
+		Queue<Location> que = new LinkedList<>();
+		boolean[][][] vst = new boolean[N + 1][M + 1][K + 1];
+		
+		que.offer(new Location(1, 1, 0, 1, true));
+		vst[1][1][0] = true;
+		
+		while(!que.isEmpty()) {
+			Location cur = que.poll();
+			int k = cur.k;
+			int dist = cur.dist;
+			boolean isDaytime = cur.isDaytime;
+			
+			if (cur.x == N && cur.y == M) return dist;
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + DX[i];
+				int ny = cur.y + DY[i];
+				
+				if (nx <= 0 || nx > N || ny <= 0 || ny > M) continue;
+				
+				if (map[nx][ny] == 0) {
+					if (!vst[nx][ny][k]) {
+						vst[nx][ny][k] = true;
+						que.offer(new Location(nx, ny, k, dist + 1, !isDaytime));
+					}
+					continue;
+				}
+				
+				if (isDaytime) { 
+					if (k < K && !vst[nx][ny][k + 1]) {
+						vst[nx][ny][k + 1] = true;
+						que.offer(new Location(nx, ny, k + 1, dist + 1, !isDaytime));
+					}	
+				} else {
+					que.offer(new Location(cur.x, cur.y, k, dist + 1, !isDaytime));
+				}
+			}
+		}
+		
+		return -1;
+	}
+}
