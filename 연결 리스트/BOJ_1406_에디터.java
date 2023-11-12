a9109f79b58c856a8524285e854ec5c254986011
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-11-12 17:19:15 +0900

    Update BOJ_1406_에디터.java

--- 연결 리스트/BOJ_1406_에디터.java
+++ 연결 리스트/BOJ_1406_에디터.java
@@ -6,47 +6,45 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		Stack<Character> left = new Stack<>();
-		Stack<Character> right = new Stack<>();
-		
+
 		String str = br.readLine();
+		LinkedList<Character> list = new LinkedList<>();
+
 		for (int i = 0; i < str.length(); i++) {
-			left.add(str.charAt(i));
+			list.add(str.charAt(i));
 		}
-		
+
+		ListIterator<Character> iterator = list.listIterator();
+		while (iterator.hasNext()) {
+			iterator.next();
+		}
+
 		int M = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			char command = st.nextToken().charAt(0);
-			char chr = ' ';
-			if (st.hasMoreTokens()) {
-				chr = st.nextToken().charAt(0);
-			}
-			
-			switch(command) {
+			String command = br.readLine();
+			switch (command.charAt(0)) {
 			case 'L':
-				if (!left.isEmpty()) right.add(left.pop());
+				if (iterator.hasPrevious()) iterator.previous();
 				break;
 			case 'D':
-				if (!right.isEmpty()) left.add(right.pop());
+				if (iterator.hasNext()) iterator.next();
 				break;
 			case 'B':
-				if (!left.isEmpty()) left.pop();
+				if (iterator.hasPrevious()) {
+					iterator.previous();
+					iterator.remove();
+				}
 				break;
 			case 'P':
-				left.add(chr);
+				iterator.add(command.charAt(2));
 			}
 		}
-		
-		for (int i = 0; i < left.size(); i++) {
-			bw.write(left.get(i));
+
+		for (char chr : list) {
+			bw.write(chr);
 		}
-		while (!right.isEmpty()) {
-			bw.write(right.pop());
-		}
-		
+
 		bw.flush();
 		bw.close();
 		br.close();
