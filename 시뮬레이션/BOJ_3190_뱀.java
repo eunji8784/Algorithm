1f53a3c3b829c81427fb83a19f1083a22b120a4f
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2023-12-03 20:42:37 +0900

    Update BOJ_3190_뱀.java

--- 시뮬레이션/BOJ_3190_뱀.java
+++ 시뮬레이션/BOJ_3190_뱀.java
@@ -5,16 +5,6 @@ public class Main {
 	private static final int[] DX = { 0, 1, 0, -1 };
 	private static final int[] DY = { 1, 0, -1, 0 };
 
-	private static class SnakeDir {
-		int time;
-		char dir;
-
-		public SnakeDir(int time, char dir) {
-			this.time = time;
-			this.dir = dir;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -26,35 +16,33 @@ public class Main {
 
 		while (k-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			board[x - 1][y - 1] = 1;
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			board[x][y] = 1;
 		}
 
 		int l = Integer.parseInt(br.readLine());
-		SnakeDir[] snakeDir = new SnakeDir[l];
+		st = new StringTokenizer(br.readLine());
+		int changeTime = Integer.parseInt(st.nextToken());
+		char changeDir = st.nextToken().charAt(0);
 
-		for (int i = 0; i < l; i++) {
-			st = new StringTokenizer(br.readLine());
-			int time = Integer.parseInt(st.nextToken());
-			char dir = st.nextToken().charAt(0);
-			snakeDir[i] = new SnakeDir(time, dir);
-		}
-
-		int time = 0, idx = 0, dir = 0;
+		int time = 0, dir = 0;
 		Deque<int[]> dq = new LinkedList<>();
 		dq.offer(new int[] { 0, 0 });
 		board[0][0] = 8;
 
 		while (true) {
-			if (idx < l) {
-				SnakeDir sd = snakeDir[idx];
-				if (time == sd.time) {
-					idx++;
-					dir = (sd.dir == 'L' ? dir + 3 : dir + 1) % 4;
+			if (time == changeTime) {
+				dir += (changeDir == 'L' ? 3 : 1);
+				dir %= 4;
+				if (l > 1) {
+					st = new StringTokenizer(br.readLine());
+					changeTime = Integer.parseInt(st.nextToken());
+					changeDir = st.nextToken().charAt(0);
+					l--;
 				}
 			}
-			
+
 			time++;
 			int[] headLoc = dq.peekLast();
 			int x = headLoc[0] + DX[dir];
@@ -78,4 +66,5 @@ public class Main {
 		bw.close();
 		br.close();
 	}
+
 }
