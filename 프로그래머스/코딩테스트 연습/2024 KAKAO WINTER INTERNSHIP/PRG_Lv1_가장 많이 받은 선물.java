9d71c8743655ef5f9ceb6efec9b0274847753838
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2024-01-25 17:25:44 +0900

    PRG_Lv1_가장 많이 받은 선물.java

---
+++ 프로그래머스/코딩테스트 연습/2024 KAKAO WINTER INTERNSHIP/PRG_Lv1_가장 많이 받은 선물.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        
+        // friend들의 index 저장
+        Map<String, Integer> map = new HashMap<>();
+        int index = 0;
+        for (String friend : friends) {
+            map.put(friend, index++);
+        }
+        
+        int n = friends.length;
+        int[][] giveAndTake = new int[n][n]; // 주고받은 선물 개수 배열
+        int[] degree = new int[n]; // 선물 지수 배열
+        
+        // 주고 받은 선물 count
+        for (String gift : gifts) {
+            String[] arr = gift.split(" ");
+            giveAndTake[map.get(arr[0])][map.get(arr[1])]++;
+        }
+        
+        // 선물 지수 계산
+        for (int i = 0; i < n; i++) {
+            int giveCount = 0, takeCount = 0;
+            for (int j = 0; j < n; j++) {
+                giveCount += giveAndTake[i][j];
+                takeCount += giveAndTake[j][i];
+            }
+            degree[i] = giveCount - takeCount;
+        }
+        
+        // 결과 계산
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (giveAndTake[i][j] > giveAndTake[j][i])  { // 준 선물이 더 많은 경우
+                    count++;
+                } else if (giveAndTake[i][j] == giveAndTake[j][i]) { // 주고 받은 선물 개수가 같은 경우
+                    if (degree[i] > degree[j]) {
+                        count++;
+                    }
+                }
+            }
+            answer = Math.max(answer, count);
+        }
+        
+        return answer;
+    }
+}
